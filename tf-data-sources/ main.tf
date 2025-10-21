16ddc130863c2fe12db7be60c0a5eda8656baa7c
adib-the-noob <mdadib550@gmail.com>
2025-10-21 22:58:06 +0600

    aws ec2-vpc-nginx added

---
+++ tf-data-sources/ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.17.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+data "aws_ami" "name" {
+  most_recent = true
+  owners      = ["amazon"]
+}
+
+output "ami_id" {
+  value = data.aws_ami.id
+}
+
+data "aws_security_group" "name" {
+  tags = {
+    Name = "MySecurtiyGroup"
+  }
+}
+
+output "aws_security_group" {
+  value = data.aws_security_group.name.id
+}
